e6e56688138369a287cf971fc22014e175235e6d
alikaklk <ali.kaklk3657@gmail.com>
2026-03-27 02:58:07 +0200

    new project

--- apps/web/src/pages/ProjectsPage/Projects.tsx
+++ apps/web/src/pages/ProjectsPage/Projects.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
                 </div>
                 <div className={styles.ProjectCard}>
                     <h2>SafeGuard Leak Checker</h2>
-                    <img src="../../../public/image.png" alt="" />
+                    <img className={styles.ProjectImage} src="../../../public/image.png" alt="" />
                     <p>
                         Go ve Next.js kullanılarak geliştirilmiş, yüksek performanslı bir siber güvenlik aracı. 
                         Milyonlarca satırlık veri sızıntısını saniyeler içinde tarayarak dijital güvenliğinizi kontrol eder.
