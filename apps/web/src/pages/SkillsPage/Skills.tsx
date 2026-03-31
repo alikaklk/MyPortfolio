09df39a5d933719a8c7153bb0caa580767e3d679
alikaklk <ali.kaklk3657@gmail.com>
2026-03-31 18:22:52 +0300

    new skills

--- apps/web/src/pages/SkillsPage/Skills.tsx
+++ apps/web/src/pages/SkillsPage/Skills.tsx
@@ -4,14 +4,17 @@ import {
   TypeScript, 
   ReactLight, 
   TailwindCssLight, 
-  NodeJsLight, 
+  DotNet, 
   PostgreSqlLight, 
   PythonLight, 
   Git, 
   GithubLight,
   Html,
   Css,
-  AngularLight
+  AngularLight,
+  GoLang,
+  SupabaseDark,
+  Swift,
 } from "@skill-icons/react";
 
 export default function Skills() {
@@ -23,14 +26,17 @@ export default function Skills() {
                 <div className={styles.IconsAndNameContainer}><Css className={styles.SkillsIcons}/><h3>Css</h3></div>
                 <div className={styles.IconsAndNameContainer}><JavaScript className={styles.SkillsIcons}/><h3>JavaScript</h3></div>
                 <div className={styles.IconsAndNameContainer}><TypeScript className={styles.SkillsIcons}/><h3>TypeScript</h3></div>
+                <div className={styles.IconsAndNameContainer}><Swift className={styles.SkillsIcons}/><h3>Swift</h3></div>
                 <div className={styles.IconsAndNameContainer}><AngularLight className={styles.SkillsIcons}/><h3>Angular</h3></div>
                 <div className={styles.IconsAndNameContainer}><ReactLight className={styles.SkillsIcons}/><h3>React</h3></div>
                 <div className={styles.IconsAndNameContainer}><TailwindCssLight className={styles.SkillsIcons}/><h3>TailwindCSS</h3></div>
-                <div className={styles.IconsAndNameContainer}><NodeJsLight className={styles.SkillsIcons}/><h3>NodeJs</h3></div>
+                <div className={styles.IconsAndNameContainer}><GoLang className={styles.SkillsIcons}/><h3>GoLang</h3></div>
+                <div className={styles.IconsAndNameContainer}><DotNet className={styles.SkillsIcons}/><h3>.NET</h3></div>
                 <div className={styles.IconsAndNameContainer}><PostgreSqlLight className={styles.SkillsIcons}/><h3>PostgreSQL</h3></div>
                 <div className={styles.IconsAndNameContainer}><PythonLight className={styles.SkillsIcons}/><h3>Python</h3></div>
                 <div className={styles.IconsAndNameContainer}><Git className={styles.SkillsIcons}/><h3>Git</h3></div>
                 <div className={styles.IconsAndNameContainer}><GithubLight className={styles.SkillsIcons}/><h3>GitHub</h3></div>
+                <div className={styles.IconsAndNameContainer}><SupabaseDark className={styles.SkillsIcons}/><h3>Supabase</h3></div>
             </div>
         </div>
     );
